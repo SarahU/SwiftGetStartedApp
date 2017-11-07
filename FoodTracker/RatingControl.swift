0726cc81dfbee8ea3d44150581fdce9af72b5299
Sarah Usher <saus@Sarahs-MacBook-Pro.local>
2017-11-07 17:00:12 +0000

    Creating custom control. Changed buttons to be star images. Not quite working yet.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Sarah Usher on 02/11/2017.
+//  Copyright © 2017 Sarah Usher. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
